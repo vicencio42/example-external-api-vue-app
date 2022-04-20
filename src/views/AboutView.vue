31c01629d4a3b50b6ac98dbf3879c942cec9098f
Vicencio42 <vicencio42@gmail.com>
2022-04-19 23:34:42 -0500

    update homeview, add axios and make a get request to json place holder, axios call to back end, added button for GH Oauth in HomeView, make web req to backend to get GH accesss token

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,3 +3,16 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+// import axios from "axios";
+
+export default {
+  created: function () {
+    var githubAccessToken = localStorage.getItem("github_access_token");
+    if (githubAccessToken) {
+      console.log(githubAccessToken);
+    }
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,48 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Posts!",
+      posts: [],
+    };
+  },
+  created: function () {
+    this.postsIndex();
+    this.newsIndex();
+    var code = this.$route.query.code;
+    if (code) {
+      axios.get("/auth/github/callback?code=" + code).then((response) => {
+        localStorage.setItem("github_access_token", response.data.access_token);
+        this.$router.push("/about");
+      });
+    }
+  },
+  methods: {
+    postsIndex: function () {
+      axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+        this.posts = response.data;
+        console.log(response.data);
+      });
+    },
+    newsIndex: function () {
+      axios.get("/news_api").then((response) => {
+        console.log(response.data);
+      });
+    },
   },
 };
 </script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <a href="https://github.com/login/oauth/authorize?client_id=1d23e1d0b8678f2750a8">Sign in to GitHub</a>
+    <div v-for="post in posts" v-bind:key="post.id">
+      <h2>{{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
